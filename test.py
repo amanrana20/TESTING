b3e7f321a152c763589fe727bd457ff2699e4721
Aman Rana <amanrana20@gmail.com>
2017-01-03 01:37:32 -0500

    changes to test.py

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-print('\nHello ! My name is {}.\nI am here to assist you in any way possible.\n'.format('Jaarvis'))
+print('\nHello ! My name is {}.\nI am here to assist you.\n'.format('Jaarvis v2.0'))
